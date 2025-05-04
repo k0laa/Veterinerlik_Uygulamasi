454da874f670d90ef56b504fe6085c184148436c
Buğra Yalçın <bugrayalcn1@gmail.com>
2025-05-05 01:38:43 +0300

    hayvanlarim_tab.py kod daha düzenli ve ortak yazıma dikkatli hale getirildi.

--- ui/tabs/hayvanlarim_tab.py
+++ ui/tabs/hayvanlarim_tab.py
@@ -13,29 +13,13 @@ def setup_hayvanlarim_tab(window, tab):
     add_pet_btn.setStyleSheet(NEW_BUTTON_STYLE)
     add_pet_btn.setFixedSize(150, 35)
 
-    layout = QGridLayout(tab)
-    # Hayvan kartı oluşturma
-    def create_hayvan_karti(hayvan_data):
-        hayvan_karti = HayvanKartiWidget(hayvan_data)
-        return hayvan_karti
-    # Hayvan kartlarını oluştur
-    hayvan_data_list = [("Milo", "Kedi", "British Shorthair", "Erkek", 3),
-                        ("Luna", "Köpek", "Siyam", "Dişi", 2),
-                        ("Luna", "Kedi", "Siyam", "Dişi", 2),
-                        ("Luna", "Kedi", "Siyam", "Dişi", 2),
-                        ("Luna", "Kedi", "Siyam", "Dişi", 2),
-                        ("Luna", "Kedi", "Siyam", "Dişi", 2),
-                        ("Luna", "Kedi", "Siyam", "Dişi", 2),
-                        ("Luna", "Kedi", "Siyam", "Dişi", 2),
-                        ]
-
-    for i, hayvan_data in enumerate(hayvan_data_list):
-        hayvan_karti = create_hayvan_karti(hayvan_data)
-        y = i // 3
-        layout.addWidget(hayvan_karti, y, i % 3)
+    window.hayvan_kart_layout = QGridLayout(tab)
 
     main_layout.addWidget(add_pet_btn, alignment=Qt.AlignRight)
-    main_layout.addLayout(layout)
+    main_layout.addLayout(window.hayvan_kart_layout)
     main_layout.addStretch()
+
+
+    window.hayvan_main_layout = main_layout
 
     add_pet_btn.clicked.connect(window.add_new_pet)
