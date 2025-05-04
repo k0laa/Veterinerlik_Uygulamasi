318002155a74809ba95d26ebc39a595d2aae37c5
Buğra Yalçın <bugrayalcn1@gmail.com>
2025-05-05 01:38:29 +0300

    hayvan_karti.py kod daha düzenli ve ortak yazıma dikkatli hale getirildi.

--- ui/widgets/hayvan_karti.py
+++ ui/widgets/hayvan_karti.py
@@ -7,9 +7,12 @@ from ui.styles import HAYVAN_KARTI_STYLE
 class HayvanKartiWidget(QWidget):
     def __init__(self, hasta_data, parent=None):
         super().__init__(parent)
+        self.edit_btn = None
+        self.delete_btn = None
         self.setFixedSize(450, 225)
         self.hasta_data = hasta_data
         self.setup_ui(hasta_data)
+        self.pet_id = hasta_data[0]
 
     def setup_ui(self, hasta_data):
         ana_layout = QVBoxLayout(self)
@@ -22,27 +25,27 @@ class HayvanKartiWidget(QWidget):
         # Resim
         image_label = QLabel()
         image_label.setFixedSize(140, 140)
-        image_label.setPixmap(self.get_animal_image(hasta_data[1]))
+        image_label.setPixmap(self.get_animal_image(hasta_data[2]))
         image_label.setScaledContents(True)
 
         # Sağda metinler
         right_info = QVBoxLayout()
         right_info.addStretch()
 
-        right_info.addWidget(QLabel(f"<b>Hayvan Adı:</b> {hasta_data[0]}"))
-        right_info.addWidget(QLabel(f"<b>Tür:</b> {hasta_data[1]}"))
-        right_info.addWidget(QLabel(f"<b>Cins:</b> {hasta_data[2]}"))
-        right_info.addWidget(QLabel(f"<b>Cinsiyet:</b> {hasta_data[3]}"))
-        right_info.addWidget(QLabel(f"<b>Yaş:</b> {hasta_data[4]}"))
+        right_info.addWidget(QLabel(f"<b>Hayvan Adı:</b> {hasta_data[1]}"))
+        right_info.addWidget(QLabel(f"<b>Tür:</b> {hasta_data[2]}"))
+        right_info.addWidget(QLabel(f"<b>Cins:</b> {hasta_data[3]}"))
+        right_info.addWidget(QLabel(f"<b>Cinsiyet:</b> {hasta_data[4]}"))
+        right_info.addWidget(QLabel(f"<b>Yaş:</b> {hasta_data[5]}"))
         right_info.addStretch()
 
         # Düzenleme Butonu
-        edit_btn = QPushButton("Düzenle")
-        edit_btn.setObjectName("editBtn")
+        self.edit_btn = QPushButton("Düzenle")
+        self.edit_btn.setObjectName("editBtn")
 
         # Sil Butonu
-        delete_btn = QPushButton("Sil")
-        delete_btn.setObjectName("dltBtn")
+        self.delete_btn = QPushButton("Sil")
+        self.delete_btn.setObjectName("dltBtn")
 
         container_layout = QVBoxLayout(kart_container)
 
@@ -52,13 +55,14 @@ class HayvanKartiWidget(QWidget):
         kart_layout.addLayout(right_info)
 
         button_layout = QHBoxLayout()
-        button_layout.addWidget(delete_btn)
-        button_layout.addWidget(edit_btn)
+        button_layout.addWidget(self.delete_btn)
+        button_layout.addWidget(self.edit_btn)
 
         container_layout.addLayout(kart_layout)
         container_layout.addLayout(button_layout)
 
         ana_layout.addWidget(kart_container)
+
 
     def get_animal_image(self, animal_type):
         image_paths = {"Kedi": "resources/icons/app_icon.png", "Köpek": "resources/icons/app_icon.png", "Kuş": "resources/icons/app_icon.png", }
